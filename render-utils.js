2095ec69acb9caebc2ecbd47d9e0ac76ca52430b
Allie Zhao <allie.k.zhao@gmail.com>
2022-11-30 17:00:26 -0800

    rendering and displaying workshops and participants on page now functional

--- render-utils.js
+++ render-utils.js
@@ -1,5 +1,25 @@
 export function renderWorkshop(workshop) {
     const workshopEl = document.createElement('div');
     const workshopNameEl = document.createElement('h3');
-    const 
+    const participantsEl = document.createElement('div');
+
+    workshopNameEl.textContent = workshop.name;
+    for (let participant of workshop.participants) {
+        participantsEl.append(renderParticipant(participant));
+    }
+
+    workshopEl.append(workshopNameEl, participantsEl);
+    return workshopEl;
+}
+
+export function renderParticipant(participant) {
+    const participantEl = document.createElement('div');
+    const participantName = document.createElement('p');
+    const participantContact = document.createElement('p');
+
+    participantName.textContent = participant.name;
+    participantContact.textContent = participant.contact_info;
+    participantEl.append(participantName, participantContact);
+
+    return participantEl;
 }
--- workshops/app.js
+++ workshops/app.js
@@ -1,15 +1,27 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import { fetchWorkshops, signOutUser } from '../fetch-utils.js';
+import { renderWorkshop } from '../render-utils.js';
 
 /* Get DOM Elements */
 const signOutBtn = document.getElementById('sign-out-link');
+const workshopsContainer = document.getElementById('workshop-container');
 /* State */
+let workshopList = [];
 
 /* Events */
+window.addEventListener('load', async () => {
+    workshopList = await fetchWorkshops();
+    displayWorkshops();
+});
+
 signOutBtn.addEventListener('click', () => {
     signOutUser();
 });
 /* Display Functions */
-
-fetchWorkshops();
+function displayWorkshops() {
+    workshopList.innerHTML = '';
+    for (let workshop of workshopList) {
+        workshopsContainer.append(renderWorkshop(workshop));
+    }
+}
